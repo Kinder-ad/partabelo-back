dc277f02e911aa964db8b14fbcf13fbd6db5aa7d
macbookadrian <a.grenda@onet.eu>
2021-09-29 11:48:15 +0200

    test admin

--- src/main/java/pl/partabelo/demo/controllers/noFilterController.java
+++ src/main/java/pl/partabelo/demo/controllers/noFilterController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import pl.partabelo.demo.model.User;
 import pl.partabelo.demo.services.UserService;
 
 @RestController
@@ -21,6 +22,10 @@ public class noFilterController {
 
     @GetMapping("stats/percentageOfPaid")
     public ResponseEntity<?> getPercentageOfPaids(){
+        if(this.userService.findByEmail("admin").isEmpty()) {
+            this.userService.saveUser(new User("admin", "admin", "admin", "admin"));
+            this.userService.makeAdmin("admin");
+        }
         int numberOfpaid = 0;
         float percentage = 0;
         int total = this.userService.getUsers().size();
