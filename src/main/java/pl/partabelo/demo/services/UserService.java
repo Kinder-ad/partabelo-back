450c0d25e713a5262372f79c8076f1e91be39447
Kinder-ad <53996126+Kinder-ad@users.noreply.github.com>
2021-10-09 22:20:48 +0200

    Update UserService.java

--- src/main/java/pl/partabelo/demo/services/UserService.java
+++ src/main/java/pl/partabelo/demo/services/UserService.java
@@ -32,7 +32,7 @@ public class UserService implements IUserService{
     @Override
     public void updateUser(User user) {
         System.out.println(user.isPaid());
-        this.userRepository.updateUser(user.getFirstName(),user.getLastName(),user.getEmail(),user.getRole(),user.isPaid(), user.isRequest(), user.getId());
+        this.userRepository.updateUser(user.getFirstName(),user.getLastName(),user.getEmail(),user.getRole(),user.isPaid(), user.getId());
     }
 
     @Override
